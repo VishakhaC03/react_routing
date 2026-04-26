727eb2528a36c6c342bc1f79f3f5066e25b0d713
VishakhaC03 <vishakhachinchane03@gmail.com>
2026-04-26 17:31:41 +0100

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -4,11 +4,14 @@ import { Link } from "react-router-dom";
 class Navbar extends Component {
   render() {
     return (
-      <nav style={{ padding: "10px", background: "#333" }}>
-        <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+        <>
+        
+        <nav className="bg-blue-300 p-4">
+        <Link to="/" className="text-blue-950 text-lg font-semibold">
           Student Course Portal
         </Link>
       </nav>
+      </>
     );
   }
 }
